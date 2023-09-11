608d2d2f9ef71fdf6cd1cc1fa1e339e747db658a
ofk <ofaruk_k@yahoo.com>
2023-09-11 10:42:18 +0300

    Domain classes added

---
+++ src/ACME.POC3.Domain/Invoice/InvoiceLine.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace ACME.POC3.Invoice
+{
+    public  class InvoiceLine: FullAuditedAggregateRoot<Guid>, IMultiTenant
+    {
+        public Guid InvoiceId { get; set; }
+        public Guid? ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public int UnitId { get; set; }
+        public double Price { get; set; }
+        public double DiscountRate { get; set; }
+        public double DiscountAmount { get; set; }
+        public double SubTotal { get; set; }
+        public double TotalWithDiscount { get; set; }
+        public double VatRate { get; set; }
+        public double VatAmount { get; set; }
+        public double TotalWithVat { get; set; }
+        public string TaxExcludingReason { get; set; }
+        public string TaxExcludingReasonCode { get; set; }
+        public int TaxExcludingReasonId { get; set; }
+        public string UnitCode { get; set; }
+        public string LineID { get; set; }
+        public string ProductCode { get; set; }
+        public Guid MasterClientId { get; set; }
+        public Guid? TenantId { get; set; }
+
+        public string WithholdingTaxCode { get; set; }
+        public double WithholdingTaxRate { get; set; }
+        public double WithholdingTaxAmount { get; set; }
+
+        public double TotalAmount { get; set; } 
+
+        public string ExportDeliveryCode { get; set; }
+        public string ExportTransportModeCode { get; set; }
+        public string ExportPackageNumber { get; set; }
+        public string ExportPackageType { get; set; }
+        public double ExportPackageQuantity { get; set; }
+        public string ExportPackageBrand { get; set; }
+        public string ExportCustomsCode { get; set; } //GTIP
+        public string ExportShipmentNumber { get; set; }
+        public double ExportShipmentGrossWeight { get; set; }
+        public double ExportShipmentNetWeight { get; set; }
+        public string ExportShipmentOriginCountryCode { get; set; }
+        public string ExportShipmentCountryCode { get; set; }
+        public string ExportShipmentCountryName { get; set; }
+        public string ExportShipmentCityName { get; set; }
+        public string ExportShipmentTownName { get; set; }
+        public string ExportShipmentStreetName { get; set; }
+        public string ExportShipmentBuildingName { get; set; }
+        public string ExportShipmentBuildingNumber { get; set; }
+        public string ExportShipmentPostalCode { get; set; }
+        public Guid? ProductTransactionId { get; set; }
+
+        protected InvoiceLine()
+        {
+        }
+
+        public InvoiceLine(
+            Guid id
+        ) : base(id)
+        {
+           
+        }
+    }
+}
---
+++ src/ACME.POC3.Domain/Invoice/MasterClient.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace ACME.POC3.Invoice
+{
+    public partial class MasterClient: FullAuditedAggregateRoot<Guid>, IMultiTenant
+    {
+        public int? SourceId { get; set; }
+        public int? SourceRefId { get; set; }
+        public string SourceRefGuid { get; set; }
+        public string TCKN_VN { get; set; }
+        public string Title { get; set; }
+        public string Title2 { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string TaxOffice { get; set; }
+        public int? TaxOfficeId { get; set; }
+        public string Country { get; set; }
+        public Guid CountryId { get; set; }
+        public string City { get; set; }
+        public int? CityId { get; set; }
+        public Guid PriceListId { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Town { get; set; }
+        public int? TownId { get; set; }
+        public int? ExchangeRateCalculation { get; set; } 
+        public string BuildingName { get; set; }
+        public string BuildingNumber { get; set; }
+        public string DoorNumber { get; set; }
+        public string PostCode { get; set; }
+        public string StreetName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string WebAddress { get; set; }
+        public string Address { get; set; }
+        public string SourceCode { get; set; }
+        public System.DateTime? SourceLastUpdate { get; set; }
+        public Guid? TenantId { get; set; }
+
+        public double TotalCredit { get; set; }
+        public double TotalDebit { get; set; }
+        public double TotalBalance { get; set; }
+        public double TotalCreditLimit { get; set; }
+        public double TotalDebitLimit { get; set; }
+        public double TotalBalanceLimit { get; set; }
+        public double TotalCreditLimitUsed { get; set; }
+        public double TotalDebitLimitUsed { get; set; }
+        public double TotalBalanceLimitUsed { get; set; }
+        public double TotalCreditLimitLeft { get; set; }
+        public double TotalDebitLimitLeft { get; set; }
+        public double TotalBalanceLimitLeft { get; set; }
+        public double TotalCreditLimitLeftPercent { get; set; }
+        public double TotalDebitLimitLeftPercent { get; set; }
+        public double TotalBalanceLimitLeftPercent { get; set; }
+
+        protected MasterClient()
+        {
+        }
+
+        public MasterClient(
+            Guid id
+        ) : base(id)
+        {
+         
+        }
+    }
+}
